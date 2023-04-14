6bd1131f404c77473ca4abf2167c25411ef51bb1
water <water@123.com>
2023-04-14 10:32:14 +0900

    refactor(docker): 修改暴露端口号

--- bin/docker-run.bat
+++ bin/docker-run.bat
@@ -8,6 +8,6 @@ set dbpath=%root%%USERNAME%%data%
 start /B cmd /C docker run  -it ^
     --rm ^
     --name note-api  ^
-    -p 8000:8000 ^
+    -p 6000:6000 ^
     -v %dbpath%:/root/h2/data ^
     note-api    ^

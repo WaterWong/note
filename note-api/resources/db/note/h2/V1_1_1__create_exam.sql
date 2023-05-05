a19733a8177f61642c085aaed48e25bdef2c53ad
water <water@123.com>
2023-05-05 11:04:50 +0900

    feature(exam): 简单实现测验的SQL

--- note-api/resources/db/note/h2/V1_1_1__create_exam.sql
+++ note-api/resources/db/note/h2/V1_1_1__create_exam.sql
@@ -1,20 +1,3 @@
-create table "exam"
-(
-    "id"             character varying(36)  not null
-        primary key,
-    "name" character varying(36),
-    "create_user_id" character varying(36)  not null,
-    "create_user"    character varying(32)  not null,
-    "create_time"    timestamp(6)           not null,
-    "update_user_id" character varying(36),
-    "update_user"    character varying(32),
-    "update_time"    timestamp(6)
-);
-
-comment on table "exam" is '测验';
-comment on column "exam"."name" is '测验标题';
-
-
 create table "exam_log"
 (
     "id"             character varying(36)  not null
@@ -22,15 +5,29 @@ create table "exam_log"
     "exam_id"       character varying(36) not null,
     "item_id"       character varying(36) not null,
     "result"        boolean not null ,
-    "origin"        character varying(256) not null,
-    "translate"     character varying(256) not null,
-    "answer"        character varying(256) not null
+    "question"      character varying(256) not null,
+    "answer"        character varying(256) not null,
+    "create_user_id" character varying(36)  not null,
+    "create_user"    character varying(32)  not null,
+    "create_time"    timestamp(6)           not null,
+    "update_user_id" character varying(36),
+    "update_user"    character varying(32),
+    "update_time"    timestamp(6)
 
 );
 
 comment on table "exam_log" is '测验日志';
 comment on column "exam_log"."result" is '詞條数量';
-comment on column "exam_log"."origin" is '原文';
-comment on column "exam_log"."translate" is '译文';
+comment on column "exam_log"."question" is '原文';
 comment on column "exam_log"."answer" is '答案';
 
+
+
+CREATE VIEW if not exists "v_exam" AS
+SELECT "ex"."id",
+       "ex"."name",
+       COUNT("el"."id") AS "count",
+       COUNT(CASE WHEN "el"."result"  THEN 1 ELSE 0 END) AS "score"
+FROM "exam" "ex"
+         LEFT JOIN "exam_log" "el" ON "ex"."id" = "el"."exam_id"
+GROUP BY "ex"."id", "ex"."name";

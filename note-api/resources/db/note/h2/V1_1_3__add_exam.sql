dfb9ab6f4a394dce670bf144c424de9b14684cea
water <water@123.com>
2023-05-05 16:04:33 +0900

    fix(exam): exam遗漏sql

---
+++ note-api/resources/db/note/h2/V1_1_3__add_exam.sql
@@ -0,0 +1,28 @@
+create table "exam"
+(
+    "id"             character varying(36) not null
+        primary key,
+    "name"           character varying(36),
+    "create_user_id" character varying(36) not null,
+    "create_user"    character varying(32) not null,
+    "create_time"    timestamp(6)          not null,
+    "update_user_id" character varying(36),
+    "update_user"    character varying(32),
+    "update_time"    timestamp(6)
+);
+
+comment on table "exam" is ''测验'';
+comment on column "exam"."name" is ''测验标题'';
+
+
+drop view if exists v_exam;
+CREATE VIEW if not exists "v_exam" AS
+SELECT "ex"."id",
+       "ex"."name",
+       COUNT("el"."id") AS "count",
+       COUNT(CASE WHEN "el"."result"  THEN 1 ELSE 0 END) AS "score"
+FROM "exam" "ex"
+         LEFT JOIN "exam_log" "el" ON "ex"."id" = "el"."exam_id"
+GROUP BY "ex"."id", "ex"."name";
+
+
